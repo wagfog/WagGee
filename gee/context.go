60fabcaf8c8cab1a3f02351e34d8f80cfcff8339
swag <17913285@qq.com>
2023-10-09 16:31:50 +0800

    Context

---
+++ gee/context.go
@@ -0,0 +1,75 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	Writer http.ResponseWriter
+	Req    *http.Request
+
+	Path   string
+	Method string
+
+	StatusCode int
+}
+
+func NewContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	//调用 WriteHeader() 会发送 HTTP 响应的头部信息到客户端，并且锁定了头部信息的修改。
+	//在调用 WriteHeader() 后，标准库会假设您已经设置了要发送的头部信息，并且不允许再进行更改。
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Contnt-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer) //创建一个 JSON 编码器（encoder），用于将数据编码为 JSON 格式，并写入到提供的 c.Writer 中。
+	//json.NewEncoder() 函数用于创建一个新的编码器
+	//使用 JSON 编码器，您可以将 Go 中的数据结构（如结构体、切片、映射等）编码为 JSON，以便在网络传输或持久化存储中使用。
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-TYpe", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
